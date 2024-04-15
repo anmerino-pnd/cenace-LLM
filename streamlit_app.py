0bd01b0fbec639de9be2268d5d3399310daabb34
Angel Merino <acedeno00@gmail.com>
2024-04-15 10:21:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,10 +79,8 @@ def main():
     with st.sidebar:
         st.title("Menú:")
         pdf_docs = st.file_uploader("Carge los documentos", accept_multiple_files=True)
-        if pdf_docs :
-            with open(pdf_docs.name, mode ="wb") as w:
-                w.write(pdf_docs.getvalue())
-        if st.button("Submit and Process", key="process?button"):
+        pdf_docs = os.path.join("/tmp", pdf_docs.name) if pdf_docs else None
+        if st.button("Submit and Process", key="process_button"):
             with st.spinner("Procesando..."):
                 raw_text = load_pdf(pdf_docs)
                 text_chunks = get_chunks(raw_text)
