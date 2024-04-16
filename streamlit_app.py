43758edd42c905df50687e4f5b02e8baf7394869
Angel Merino <acedeno00@gmail.com>
2024-04-15 21:08:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,8 +82,8 @@ def main():
         pdf_docs = st.file_uploader("Cargue los documentos", accept_multiple_files=True, key="str")
         if st.button("Submit and Process", key="process_button"):
             with st.spinner("Procesando..."):
-                print(pdf_docs)
-                print(type(pdf_docs))
+                st.code(print(pdf_docs))
+                st.code(print(type(pdf_docs)))
                 raw_text = load_pdf(pdf_docs)
                 text_chunks = get_chunks(raw_text)
                 get_vector_store(text_chunks)
