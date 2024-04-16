6e8c1e422415101ffd1d2775cfd09c3a45356617
Angel Merino <acedeno00@gmail.com>
2024-04-16 13:15:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,21 +61,33 @@ def get_conversational_chain(VectorStore, retriever):
     memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages= True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
-        retriever = retriever,
         memory = memory
     )
     return conversation_chain
 
+def handle_user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content, unsafe_allow_html=True))
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content, unsafe_allow_html=True))
 
 def main():
     st.set_page_config(page_title="Chatbot", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     st.header("Chatbot")
-    st.text_input("Haga una pregunta sobre sus documentos")
+    user_question = st.text_input("Haga una pregunta sobre sus documentos")
+    if user_question:
+        handle_user_input(user_question)
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
     
     # st.write(user_template, unsafe_allow_html=True)
     # st.write(bot_template, unsafe_allow_html=True)
