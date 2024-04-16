eaef7e0847af04147bd3431a912c1d51338729e1
Angel Merino <acedeno00@gmail.com>
2024-04-15 21:31:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,53 +23,46 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
-st.set_page_config(page_title="Langchain", page_icon="🔗", layout="wide")
+# def load_pdf(file):
+#     """Load a PDF file and split it into pages."""
+#     pdf_loader = PyPDFLoader(file)
+#     page = pdf_loader.load_and_split()
+#     return page
 
-st.markdown("""
-1. **Upload a PDF file**
-2. **Ask a question**
-# Langchain""")
+# def get_chunks(page):
+#     """the text is split into chunks of 1000 characters each."""
+#     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+#     chunks = text_splitter.split_documents(page)
+#     return chunks
 
-def load_pdf(file):
-    """Load a PDF file and split it into pages."""
-    pdf_loader = PyPDFLoader(file)
-    page = pdf_loader.load_and_split()
-    return page
+# def get_vector_store(chunks):
+#     """Get vectors for each chunk."""
+#     embeddings = OllamaEmbeddings(model='nomic-embed-text:latest') 
+#     vector_store = FAISS.from_documents(chunks, embeddings)
+#     vector_store.save_local("Character_FAISS_nomic")
 
-def get_chunks(page):
-    """the text is split into chunks of 1000 characters each."""
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    chunks = text_splitter.split_documents(page)
-    return chunks
+# def get_conversational_chain():
+#     """Get a conversation prompt and response."""
+#     llm = Ollama(model='gemma:2b')
+#     chat_prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
 
-def get_vector_store(chunks):
-    """Get vectors for each chunk."""
-    embeddings = OllamaEmbeddings(model='nomic-embed-text:latest') 
-    vector_store = FAISS.from_documents(chunks, embeddings)
-    vector_store.save_local("Character_FAISS_nomic")
+# <context>
+# {context}
+# </context>
 
-def get_conversational_chain():
-    """Get a conversation prompt and response."""
-    llm = Ollama(model='gemma:2b')
-    chat_prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+# Question: {input}""")
+#     chat_output_parser = StrOutputParser()
+#     document_chain = create_stuff_documents_chain(llm, chat_prompt, chat_output_parser)
+#     return document_chain
 
-<context>
-{context}
-</context>
-
-Question: {input}""")
-    chat_output_parser = StrOutputParser()
-    document_chain = create_stuff_documents_chain(llm, chat_prompt, chat_output_parser)
-    return document_chain
-
-def user_input(user_question):
-    """Get user input and return the response."""
-    embeddings = OllamaEmbeddings(model='nomic-embed-text:latest')
-    new_vector_store = FAISS.load_local("Character_FAISS_nomic", embeddings)
-    chain = get_conversational_chain()
-    retriever = new_vector_store.as_retriever()
-    retrieval_chain = create_retrieval_chain(retriever, chain)
-    st.write(retrieval_chain.invoke({"input": user_question}))
+# def user_input(user_question):
+#     """Get user input and return the response."""
+#     embeddings = OllamaEmbeddings(model='nomic-embed-text:latest')
+#     new_vector_store = FAISS.load_local("Character_FAISS_nomic", embeddings)
+#     chain = get_conversational_chain()
+#     retriever = new_vector_store.as_retriever()
+#     retrieval_chain = create_retrieval_chain(retriever, chain)
+#     st.write(retrieval_chain.invoke({"input": user_question}))
 
 def main():
     st.set_page_config(page_title="Chatbot", page_icon=":books:")
